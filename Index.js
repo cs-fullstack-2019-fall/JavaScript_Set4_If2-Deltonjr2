a611699e8d59a2a1397fdd312829395f5ef637a5
Deltonjr2 <Demetria.Farewell@yahoo.com>
2019-08-07 17:02:21 -0500

    Partial Completion of question 11

---
+++ Index.js
@@ -0,0 +1,30 @@
+// JavaScript_Classwork4_If2
+//
+// Create a new file for each exercise
+//
+// Exercise 10
+//
+// Ask the user for 3 cards. If the sum equals 21 print BLACKJACK! If the sum is greater than 21 print BUST! If the sum is less than 21 print “The total is [TOTAL]”
+//
+// Exercise 11
+// Ask the user to enter their grade. Print their letter grade:
+//
+//     A 90 - 100 B 80 - 89 C 70 - 79 D 60 - 69 F 0 - 59
+//
+// Extra Credit:
+//     If it's negative or over 100 print an error(.
+//
+// Exercise 1
+// Ask the user to enter their age. Check if they entered a value between 0 and 125. If valid, print "REAL AGE". If invalid print “NOT A REAL AGE!!!”
+
+var Cardrequest1 = parseInt( prompt("Ask for a number card"));
+alert (Cardrequest1);
+
+var Cardrequest2= parseInt( prompt("Ask for a number card"));
+alert (Cardrequest2);
+
+var Cardrequest3= parseInt( prompt("Ask for a number card"));
+alert (Cardrequest3);
+
+alert (Cardrequest1 + Cardrequest2 + Cardrequest3);
+
